ab7e4853a7a9d40b4a552b97604c4cb4c515090f
Raoul Schram <r.d.schram@uu.nl>
2022-06-22 15:27:59 +0200

    Fix inclusion of schema file in distribution

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     description='Package for creating synthetic datasets from datasets',
     long_description='Package for creating synthetic datasets from datasets',
     packages=find_packages(exclude=['data', 'docs', 'tests', 'examples']),
+    include_package_data=True,
     python_requires='~=3.7',
     install_requires=[
         "pandas",
