d53db4ded12baf34b207be571e4f7597ebee7ce4
Raoul Schram <r.d.schram@uu.nl>
2022-08-05 14:02:28 +0200

    remove trailing whitespace

--- metasynth/dataset.py
+++ metasynth/dataset.py
@@ -76,7 +76,7 @@ class MetaDataset():
             unique
 
             To set a column to be unique/key.
-            This is only available for the integer and string datatypes. Setting a variable 
+            This is only available for the integer and string datatypes. Setting a variable
             to unique ensures that the synthetic values generated for this variable are unique.
             This is useful for ID or primary key variables, for example. The parameter...
             is ignored when the distribution is set manually. For example:
