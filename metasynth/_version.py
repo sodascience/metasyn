03e5cf0ecc1b9b60956e7048a364cf50d91f612e
Raoul Schram <r.d.schram@uu.nl>
2022-08-12 14:56:08 +0200

    Fix versioneer setup

--- metasynth/_version.py
+++ metasynth/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "metasynth"
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "metasynth-"
     cfg.versionfile_source = "metasynth/_version.py"
     cfg.verbose = False
