8ad08ce31579277ebca0657a222ead7d35c24183
Raoul Schram <r.d.schram@uu.nl>
2022-09-13 12:04:17 +0200

    Fix linting issues

--- metasynth/distribution/datetime.py
+++ metasynth/distribution/datetime.py
@@ -12,6 +12,17 @@ from metasynth.distribution.base import ScipyDistribution, DateDistribution
 
 
 def convert_numpy_datetime(time_obj: np.datetime64) -> dt.datetime:
+    """Convert numpy datetime to python stdlib datetime.
+
+    Parameters
+    ----------
+    time_obj: datetime to be converted.
+
+    Returns
+    -------
+    datetime.datetime:
+        Converted datetime.
+    """
     unix_epoch = np.datetime64(0, 's')
     one_second = np.timedelta64(1, 's')
     seconds_since_epoch = (time_obj - unix_epoch) / one_second
