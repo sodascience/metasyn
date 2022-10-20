78ca8687210524989e8f17aee36f452c26e3f8a8
Raoul Schram <r.d.schram@uu.nl>
2022-10-20 18:19:34 +0200

    Fix randomized inputs

--- examples/utils.py
+++ examples/utils.py
@@ -4,6 +4,7 @@ from metasynth.distribution.datetime import UniformDateTimeDistribution, Uniform
 from metasynth.distribution.datetime import UniformDateDistribution
 import wget
 from pathlib import Path
+import random
 
 
 def get_demonstration_fp():
@@ -14,21 +15,23 @@ def get_demonstration_fp():
     if not titanic_fp.is_file():
         wget.download("https://raw.githubusercontent.com/pandas-dev/pandas/main/doc/data/titanic.csv")
     df = pd.read_csv(titanic_fp)
+    np.random.seed(1283742)
+    random.seed(1928374)
 
     # Convert Age to a nullable integer.
     df["Age"] = df["Age"].round().astype("Int64")
 
     # Add a date column.
-    date_dist = UniformDateDistribution._example_distribution()
+    date_dist = UniformDateDistribution.default_distribution()
     df["Birthday"] = [date_dist.draw() if np.random.rand() < 0.9 else pd.NA for _ in range(len(df))]
 
     # Add a time column.
 
-    time_dist = UniformTimeDistribution._example_distribution()
+    time_dist = UniformTimeDistribution.default_distribution()
     df["Board time"] = [time_dist.draw() if np.random.rand() < 0.9 else pd.NA for _ in range(len(df))]
 
     # Add a datetime column
-    time_dist = UniformDateTimeDistribution._example_distribution()
+    time_dist = UniformDateTimeDistribution.default_distribution()
     df["Married since"] = [time_dist.draw() if np.random.rand() < 0.9 else pd.NA for _ in range(len(df))]
 
     df["all_NA"] = [pd.NA for _ in range(len(df))]
