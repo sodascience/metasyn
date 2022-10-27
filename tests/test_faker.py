f8a4dd7d9887fac49bfd6e2b3dbd52613fc54eb6
Raoul Schram <r.d.schram@uu.nl>
2022-10-27 16:55:06 +0200

    Add polars to tests

--- tests/test_faker.py
+++ tests/test_faker.py
@@ -1,10 +1,13 @@
 import pandas as pd
+import polars as pl
+from pytest import mark
 
 from metasynth.distribution.faker import FakerDistribution
 
 
-def test_faker():
-    var = FakerDistribution.fit(pd.Series([1, 2, 3]))
+@mark.parametrize("series_type", [pd.Series, pl.Series])
+def test_faker(series_type):
+    var = FakerDistribution.fit(series_type([1, 2, 3]))
     assert isinstance(var.to_dict(), dict)
     assert isinstance(var.draw(), str)
     assert var.is_named("faker.city.nl_NL")
