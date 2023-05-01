db7f6699e4d7218af9dbbeccaade8f0b3858438d
Raoul Schram <r.d.schram@uu.nl>
2023-05-01 11:45:39 +0200

    Fix test

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -6,7 +6,7 @@ from metasynth.distribution.continuous import UniformDistribution,\
 from metasynth.distribution.faker import FakerDistribution
 from metasynth.distribution.regex import RegexDistribution
 from metasynth.distribution.discrete import DiscreteUniformDistribution
-from metasynth.provider import get_distribution_provider
+from metasynth.provider import DistributionProviderList
 
 
 @mark.parametrize(
@@ -22,10 +22,10 @@ from metasynth.provider import get_distribution_provider
 )
 def test_util(dist_str, dist):
     provider_list = DistributionProviderList("builtin")
-    dist_class = dist_tree.find_distribution(dist_str)
+    dist_class = provider_list._find_distribution(dist_str)
     assert dist == dist_class
     if dist_str.startswith("faker"):
         with raises(ValueError):
-            dist_tree.find_distribution("this is not a distribution")
-    new_class = dist_tree.find_distribution(dist_class.__name__)
+            provider_list._find_distribution("this is not a distribution")
+    new_class = provider_list._find_distribution(dist_class.__name__)
     assert new_class == dist_class
