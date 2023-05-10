3d0905ceb9c48324e23553213af363c5761d5fb8
Raoul Schram <r.d.schram@uu.nl>
2023-05-10 13:30:05 +0200

    Update tests for API change

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -22,10 +22,10 @@ from metasynth.provider import DistributionProviderList
 )
 def test_util(dist_str, dist):
     provider_list = DistributionProviderList("builtin")
-    dist_class = provider_list._find_distribution(dist_str)
+    dist_class = provider_list.find_distribution(dist_str)
     assert dist == dist_class
     if dist_str.startswith("faker"):
         with raises(ValueError):
-            provider_list._find_distribution("this is not a distribution")
-    new_class = provider_list._find_distribution(dist_class.__name__)
+            provider_list.find_distribution("this is not a distribution")
+    new_class = provider_list.find_distribution(dist_class.__name__)
     assert new_class == dist_class
