030e616864f65b962cb4058570d529106fc1a76e
qubixes <44498096+qubixes@users.noreply.github.com>
2023-05-25 09:13:45 +0200

    Update tutorial and add google colab badge

--- metasynth/__init__.py
+++ metasynth/__init__.py
@@ -9,6 +9,7 @@ from importlib.metadata import version
 
 from metasynth.dataset import MetaDataset
 from metasynth.var import MetaVar
+from metasynth.demo.dataset import demo_file
 
-__all__ = ["MetaVar", "MetaDataset"]
+__all__ = ["MetaVar", "MetaDataset", "demo_file"]
 __version__ = version("metasynth")
