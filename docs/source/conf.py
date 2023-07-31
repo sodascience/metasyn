1e191861b08b68ab37a5c05b9dc1fc6c00fc218c
Samuwhale <sfspithorst@gmail.com>
2023-07-31 11:23:21 +0200

    Implemented copy buttons to code blocks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ release = '0.3'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.napoleon', "sphinx.ext.autodoc", "sphinx_autodoc_typehints",
-              'sphinx_rtd_theme', "sphinx_inline_tabs",]
+              'sphinx_rtd_theme', "sphinx_inline_tabs", "sphinx_copybutton"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
