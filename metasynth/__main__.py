4ccd90444d46df6fbe1e4fc9bb0b9c99be7cb900
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2023-08-11 14:24:58 +0200

    First version of CLI (#142)
    MetaSynth now has a command line interface, so that it can create synthetic dataset using "metasynth synthesize [command line arguments]."

---
+++ metasynth/__main__.py
@@ -0,0 +1,93 @@
+"""CLI for generating synthetic data frames from a metasynth .json file."""
+import argparse
+import pathlib
+import pickle
+import sys
+
+from metasynth._version import __version__
+from metasynth import MetaFrame
+
+
+def main():
+    """Parse arguments and generate synthetic data."""
+    parser = argparse.ArgumentParser(
+        prog="metasynth",
+        description="Synthesize data from Generative Metadata Format .json file.",
+        usage="%(prog)s synthesize [options] input output"
+    )
+    parser.add_argument(
+        "input",
+        help="input file; typically .json adhering to the Generative Metadata Format",
+        type=pathlib.Path
+    )
+    parser.add_argument(
+        "output",
+        help="output file (.csv, .feather, .parquet, .pkl, or .xlsx)",
+        nargs="?",
+        type=pathlib.Path
+    )
+    parser.add_argument(
+        "-n", "--num_rows",
+        help="number of rows to synthesize",
+        default=None,
+        type=int,
+        required=False
+    )
+    parser.add_argument(
+        "-p", "--preview",
+        help="preview six-row synthesized data frame in console and exit",
+        action="store_true"
+    )
+    # version
+    parser.add_argument(
+        "-v", "--version",
+        action="version",
+        version=f"%(prog)s {__version__}",
+    )
+
+    # pop the shim subcommand and check that it is "synthesize"
+    subcommand = sys.argv.pop(1)
+    if subcommand != "synthesize":
+        parser.error(f"Invalid subcommand ({subcommand}).")
+
+    # parse the args without the shim subcommand
+    args, _ = parser.parse_known_args()
+
+    if not args.preview and not args.output:
+        parser.error("Output file is required.")
+
+    # Create the metaframe from the json file
+    meta_frame = MetaFrame.from_json(args.input)
+
+    if args.preview:
+        # only print six rows and exit
+        print(meta_frame.synthesize(6))
+        return
+
+    # Generate a data frame
+    if args.num_rows is not None:
+        data_frame = meta_frame.synthesize(args.num_rows)
+    else:
+        data_frame = meta_frame.synthesize(meta_frame.n_rows)
+
+    # Store the dataframe to file
+    if args.output.suffix == ".csv":
+        data_frame.write_csv(args.output)
+    elif args.output.suffix == ".feather":
+        data_frame.write_ipc(args.output)
+    elif args.output.suffix == ".parquet":
+        data_frame.write_parquet(args.output)
+    elif args.output.suffix == ".xlsx":
+        data_frame.write_excel(args.output)
+    elif args.output.suffix == ".pkl":
+        with open(args.output, "wb") as pkl_file:
+            pickle.dump(data_frame, file=pkl_file)
+    else:
+        parser.error(
+            f"Unsupported output file format ({args.output.suffix})." +
+            "Use .csv, .feather, .parquet, .pkl, or .xlsx."
+        )
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_cli.py
@@ -0,0 +1,57 @@
+import sys
+import subprocess
+from pathlib import Path
+from pytest import mark, fixture
+import polars as pl
+from metasynth import MetaFrame
+
+TMP_DIR_PATH = None
+
+@fixture(scope="module")
+def tmp_dir(tmp_path_factory) -> Path:
+    global TMP_DIR_PATH
+    if TMP_DIR_PATH is None:
+        # Create a temporary input file
+        TMP_DIR_PATH = tmp_path_factory.mktemp("data")
+        json_path = TMP_DIR_PATH / "titanic.json"
+        csv_fp = Path("tests", "data", "titanic.csv")
+        csv_dt = {
+            "PassengerId": pl.Int64,
+            "Survived": pl.Categorical,
+            "Pclass": pl.Categorical,
+            "Name": str,
+            "Sex": pl.Categorical,
+            "SibSp": pl.Categorical,
+            "Parch": pl.Categorical,
+            "Ticket": str,
+            "Cabin": str,
+            "Embarked": pl.Categorical,
+            "Age": float,
+            "Fare": float
+        }
+        data_frame = pl.read_csv(csv_fp, dtypes=csv_dt)[:100]
+        meta_frame = MetaFrame.fit_dataframe(data_frame, spec={"PassengerId": {"unique": True}})
+        meta_frame.to_json(json_path)
+    return TMP_DIR_PATH
+
+@mark.parametrize("ext", [".csv", ".feather", ".parquet", ".pkl", ".xlsx"])
+def test_cli(tmp_dir, ext):
+    """A simple integration test for reading and writing using the CLI"""
+    
+    # create out file path with correct extension
+    out_file = tmp_dir / f"titanic{ext}"
+
+    # create command to run in subprocess with arguments
+    cmd = [
+        Path(sys.executable).resolve(),   # the python executable
+        Path("metasynth", "__main__.py"), # the cli script
+        "synthesize",                     # the subcommand
+        "-n 25",                          # only generate 25 samples
+        tmp_dir / "titanic.json",         # the input file
+        out_file                          # the output file
+    ]
+
+    # Run the cli with different extensions
+    result = subprocess.run(cmd, check=False)
+    assert result.returncode == 0
+    assert out_file.is_file()
