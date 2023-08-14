1d9d3c0dbc069970623cb58f01f222a39bc88ccf
Raoul Schram <r.d.schram@uu.nl>
2023-08-14 16:35:35 +0200

    Add new unique faker distribution

--- metasynth/distribution/faker.py
+++ metasynth/distribution/faker.py
@@ -59,3 +59,26 @@ class FakerDistribution(BaseDistribution):
             "faker_type": {"type": "string"},
             "locale": {"type": "string"},
         }
+
+
+@metadist(implements="core.unique_faker", var_type="string")
+class UniqueFakerDistribution(FakerDistribution):
+    def __init__(self, faker_type: str, locale: str = "en_US"):
+        super().__init__(faker_type, locale)
+        self.key_set: set[str] = set()
+
+    def draw_reset(self):
+        self.key_set = set()
+
+    def draw(self) -> str:
+        n_try = 0
+        while n_try < 1e5:
+            new_val = super().draw()
+            if new_val not in self.key_set:
+                self.key_set.add(new_val)
+                return new_val
+            n_try += 1
+        raise ValueError("Failed to draw unique string after 100.000 tries.")
+
+    def information_criterion(self, values):
+        return 99999
