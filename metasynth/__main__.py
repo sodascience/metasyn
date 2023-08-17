2db881c2fd2965905a885f7e47cc4fa9142f326f
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2023-08-17 17:04:16 +0200

    Add jsonschema entrypoint to CLI
    - Overhauled argument parsing for CLI
    - Better error message for top-level CLI call
    - Automatic dispatch to different entrypoints in __main__.py file

--- metasynth/__main__.py
+++ metasynth/__main__.py
@@ -3,17 +3,57 @@ import argparse
 import pathlib
 import pickle
 import sys
+import json
 
-from metasynth._version import __version__
+try:
+    from importlib_metadata import entry_points
+except ImportError:
+    from importlib.metadata import entry_points  # type: ignore
+
+from metasynth._version import version_tuple
 from metasynth import MetaFrame
+from metasynth.validation import create_schema
+
+SEMVER = f"{version_tuple[0]}.{version_tuple[1]}.{version_tuple[2]}"
+
+MAIN_HELP_MESSAGE = f"""
+Metasynth CLI version {SEMVER}
+
+Usage: metasynth [subcommand] [options]
+
+Available subcommands:
+    synthesize - generate synthetic data from a .json file
+    jsonschema - generate json schema from distribution providers
+
+Program information:
+    -v, --version - display CLI version and exit
+    -h, --help    - display this help file and exit
+"""
+
+ENTRYPOINTS = ["synthesize", "jsonschema"]
+
+def main() -> None:
+    """CLI pointing to different entrypoints."""
+    # show help by default, else consume first argument
+    subcommand = "--help" if len(sys.argv) < 2 else sys.argv.pop(1)
+
+    if subcommand in ["-h", "--help"]:
+        print(MAIN_HELP_MESSAGE)
+    elif subcommand in ["-v", "--version"]:
+        print(f"Metasynth CLI version {SEMVER}")
+    elif subcommand in ENTRYPOINTS:
+        # find the subcommand in this module and run it!
+        getattr(sys.modules[__name__], subcommand)()
+    else:
+        print(f"Invalid subcommand ({subcommand}). For help see metasynth --help")
+        sys.exit(1)
 
 
-def main():
-    """Parse arguments and generate synthetic data."""
+def synthesize() -> None:
+    """Program to generate synthetic data."""
     parser = argparse.ArgumentParser(
-        prog="metasynth",
-        description="Synthesize data from Generative Metadata Format .json file.",
-        usage="%(prog)s synthesize [options] input output"
+        prog="metasynth synthesize",
+        description="Synthesize data from Generative Metadata Format .json file."
     )
     parser.add_argument(
         "input",
@@ -38,19 +78,9 @@ def main():
         help="preview six-row synthesized data frame in console and exit",
         action="store_true"
     )
-    # version
-    parser.add_argument(
-        "-v", "--version",
-        action="version",
-        version=f"%(prog)s {__version__}",
-    )
 
-    # pop the shim subcommand and check that it is "synthesize"
-    subcommand = sys.argv.pop(1)
-    if subcommand != "synthesize":
-        parser.error(f"Invalid subcommand ({subcommand}).")
 
-    # parse the args without the shim subcommand
+    # parse the args without the subcommand
     args, _ = parser.parse_known_args()
 
     if not args.preview and not args.output:
@@ -68,7 +98,7 @@ def main():
     if args.num_rows is not None:
         data_frame = meta_frame.synthesize(args.num_rows)
     else:
-        data_frame = meta_frame.synthesize(meta_frame.n_rows)
+        data_frame = meta_frame.synthesize(meta_frame.n_rows) # type: ignore
 
     # Store the dataframe to file
     if args.output.suffix == ".csv":
@@ -87,6 +117,45 @@ def main():
             f"Unsupported output file format ({args.output.suffix})." +
             "Use .csv, .feather, .parquet, .pkl, or .xlsx."
         )
+
+def jsonschema() -> None:
+    """Program to generate json schema from dist providers."""
+    parser = argparse.ArgumentParser(
+        prog="metasynth jsonschema",
+        description="Create Generative Metadata Format jsonschema and print to console."
+    )
+
+    parser.add_argument(
+        "plugins",
+        help="Plugins to include in the generated schema (default none)",
+        nargs="*"
+    )
+
+    parser.add_argument(
+        "-a", "--avail",
+        help="display available modules and quit",
+        action="store_true"
+    )
+
+    # parse the args without the subcommand
+    args, _ = parser.parse_known_args()
+
+    # deduplicated list of plugins for schema
+    plugins_avail = {entry.name for entry in entry_points(group="metasynth.distribution_provider")}
+
+    if args.avail:
+        _ = [print(a) for a in plugins_avail if a != "builtin"]
+        return
+
+    plugins = {"builtin", *args.plugins}
+    if len(plugins - plugins_avail) > 0:
+        notfound = ", ".join(plugins - plugins_avail)
+        pl_avail = ", ".join(plugins_avail - {"builtin"})
+        errmsg = f"\n  Requested plugin(s) not found: {notfound}" + \
+            f"\n  Available plugins: {pl_avail}"
+        parser.error(errmsg)
+    schema = create_schema(list(plugins))
+    print(json.dumps(schema, indent = 2))
 
 
 if __name__ == "__main__":
