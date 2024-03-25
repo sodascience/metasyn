e48102589782f6870138da3889f5bcaeeec44e70
Raoul Schram <r.d.schram@uu.nl>
2024-03-25 16:04:45 +0100

    Prepare for new release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ author = 'SODA Team'
 
 
 # The full version, including alpha/beta/rc tags
-release = '0.7.1'
+release = '0.8.0'
 
 
 # -- General configuration ---------------------------------------------------
