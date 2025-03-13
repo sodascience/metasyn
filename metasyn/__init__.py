a191e8c42fa48b1c64fe01b18b88074281ebfb69
Raoul Schram <r.d.schram@uu.nl>
2025-03-13 17:23:10 +0100

    Fix import order

--- metasyn/__init__.py
+++ metasyn/__init__.py
@@ -22,10 +22,10 @@ from importlib.metadata import version
 
 from metasyn.demo.dataset import demo_dataframe, demo_file
 from metasyn.distribution.base import metadist
+from metasyn.filereader import read_csv, read_sav
 from metasyn.metaframe import MetaFrame
 from metasyn.var import MetaVar
 from metasyn.varspec import VarSpec
-from metasyn.filereader import read_csv, read_sav
 
 __all__ = ["MetaVar", "MetaFrame", "demo_file", "demo_dataframe", "metadist", "VarSpec",
            "read_csv", "read_sav"]
