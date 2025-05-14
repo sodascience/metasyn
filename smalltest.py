20d6683c1bc964eef079fd1cc748e76bcb13eeb0
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2025-05-14 09:39:03 +0200

    Delete smalltest.py

--- smalltest.py
+++
@@ -1,6 +0,0 @@
-from metasyn import MetaFrame, VarSpec, demo_dataframe
-
-df = demo_dataframe("hospital")
-vs = [VarSpec(name="patient_id", unique=True)]
-mf = MetaFrame.fit_dataframe(df, var_specs=vs)
-mf.synthesize(30000)
