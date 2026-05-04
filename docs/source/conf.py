b445b5691bbf0183b40661d3e1075b617531f8c7
Raoul Schram <r.d.schram@uu.nl>
2026-05-04 11:19:19 +0200

    Bump version to 2.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ author = "SoDa Team"
 
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.1.0'
 
 # -- General configuration ---------------------------------------------------
 
