771f3eade4b2a235d997488a8d4ab900b85a4462
Raoul Schram <r.d.schram@uu.nl>
2026-09-15 18:04:35 +0200

    Fix notebook + allow download of GMF/CSV

--- examples/create_synthetic.py
+++ examples/create_synthetic.py
@@ -1,7 +1,15 @@
 import marimo
 
 __generated_with = "0.24.0"
-app = marimo.App()
+app = marimo.App(width="full", app_title="metasyn")
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    # Metasyn - generate synthetic data
+    """)
+    return
 
 
 @app.cell
@@ -14,6 +22,14 @@ def _():
     from collections import defaultdict
 
     return DistributionRegistry, MetaFrameBuilder, defaultdict, mo, np
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Set the size of the synthetic table. Warning: changing the number of columns will reset the specification of the columns.
+    """)
+    return
 
 
 @app.cell
@@ -58,6 +74,13 @@ def _(col_form, mo):
         set_param_state(lambda cur: [
             {x: y for x, y in v.items() if x in all_names} | {param_name: new_val} if idx == i else v for idx, v in enumerate(cur)
         ])
+    def update_param_defaults(i, default_params):
+        cur_params = get_param_state()[i]
+        if list(cur_params) == list(default_params) and [type(x) for x in cur_params.values()] == [type(x) for x in default_params.values()]:
+            return
+        set_param_state(lambda cur: [
+            default_params if idx==i else v for idx, v in enumerate(cur)
+        ])
 
     return (
         get_dist_state,
@@ -67,6 +90,7 @@ def _(col_form, mo):
         get_var_state,
         update_dist_state,
         update_name_state,
+        update_param_defaults,
         update_param_state,
         update_unq_state,
         update_var_state,
@@ -122,7 +146,7 @@ def _(DistributionRegistry, defaultdict):
             else:
                 res_dict[dist.name].extend(dist.var_type)
         return res_dict
-    
+
 
     return get_distributions, reg
 
@@ -151,28 +175,37 @@ def _(
 
 
 @app.cell
-def _(mo, np, reg, update_param_state):
+def _(mo, np, reg, update_param_defaults, update_param_state):
     def get_parameters(dist_name, var_type, unique, idx):
         try:
             dist = reg.find_distribution(dist_name, var_type=var_type, unique=unique)
         except ValueError:
             return []
         params = dist.default_distribution().to_dict()["parameters"]
+        if "regex_data" in params:
+            params = {"regex_data": params["regex_data"]["regex"]}
 
         cur_param_form = []
         #cur_col_name = name_forms[m_col].value
+        update_param_defaults(idx, params)
         for key, val in params.items():
             on_change = lambda v, idx=idx, param_name=key: update_param_state(idx, param_name, v, list(params))
-
-            if isinstance(params[key], (str, dict)):
+            if isinstance(val, str) and dist.var_type == "datetime" and key in ["lower", "upper", "value"]:
+                cur_param_form.append(mo.ui.datetime(value=val, label=key + ": ", on_change=on_change))
+            elif isinstance(params[key], str):
                 cur_param_form.append(mo.ui.text(value=val, label=key + ": " , on_change=on_change))
             elif isinstance(params[key], bool):
                 cur_param_form.append(mo.ui.checkbox(value=val, label=key, on_change=on_change))
             elif isinstance(params[key], (float, int)):
                 cur_param_form.append(mo.ui.number(value = val, label=key + ": ", on_change=on_change))
             elif isinstance(params[key], np.ndarray):
-                continue
-                cur_param_form.append(mo.ui.array(label=key + ": ", on_change=on_change))
+                if key == "probs":
+                    array_change = lambda v, idx=idx, param_name=key: update_param_state(idx, param_name, [float(x.strip()) for x in v.split(",")], list(params))
+                else:
+                    array_change = lambda v, idx=idx, param_name=key: update_param_state(idx, param_name, v.split(","), list(params))
+
+                cur_param_form.append(mo.ui.text(value=",".join(str(x) for x in val), label=key + ": ", on_change=array_change))
+            
             else:
                 raise ValueError("Not implemented type: ", type(params[key]), params[key])
         return cur_param_form
@@ -200,10 +233,26 @@ def _(
     return (param_array,)
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Create columns with distributions
+
+    To generate synthetic data select the following for each column:
+
+    - Name of the column
+    - Type of the variable, e.g. string, discrete, time.
+    - Uniqueness, whether the column has unique values.
+    - Distribution, such as normal, exponential. The available distributions are only shown after selecting the variable type.
+    - Parameters of the distribution.
+    """)
+    return
+
+
 @app.cell
-def _(dist_array, mo, name, param_array, unq_array, var_array):
+def _(dist_array, mo, name_array, param_array, unq_array, var_array):
     mo.vstack([
-        mo.hstack(name),
+        mo.hstack(name_array),
         mo.hstack(var_array),
         mo.hstack(unq_array),
         mo.hstack(dist_array),
@@ -226,10 +275,79 @@ def _(
     builder = MetaFrameBuilder()
     builder.n_rows = row_form.value
     for col_id in range(col_form.value):
-        builder.add_column(name=name_array[col_id].value, var_type=var_array[col_id].value)
-        builder[name_array[col_id].value].distribution = {"name": dist_array[col_id].value, "unique": unq_array[col_id].value, "parameters": get_param_state()[col_id]}
-    mf = builder.fit()
-    mf.synthesize()
+        name = name_array[col_id].value
+        name = f"Column {col_id}" if name == "" else name
+        var_type = var_array[col_id].value
+        dist_name = dist_array[col_id].value
+        unique = unq_array[col_id].value
+        parameters = get_param_state()[col_id]
+        if var_type is None or dist_name is None or parameters is None:
+            var_type = "string"
+            dist_name = "na"
+            parameters = {}
+            unique = False
+        builder.add_column(name=name, var_type=var_type)
+        builder[name].distribution = {"name": dist_name, "unique": unique, "parameters": parameters}
+    mf = builder.fit(progress_bar=False)
+    mf.synthesize(progress_bar=False)
+    return (mf,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Generate a GMF file
+
+    Below you can generate a GMF (Generative Metadata Format) file. This file can be used to generate synthetic data. You can also generate synthetic data immediately one paragraph below.
+    """)
+    return
+
+
+@app.cell
+def _(mf, mo):
+    from metasyn.metaframe import _jsonify
+    import json
+
+    def _to_json(mf):
+        mf_dict = _jsonify(mf.to_dict())
+        return json.dumps(mf_dict, indent=4).encode("utf-8")
+    
+
+    mo.download(
+        data=_to_json(mf),
+        filename="gmf.json",
+        mimetype="application/json",
+        label="Generate GMF file"
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Generate a CSV file
+
+    With the button below you can download the CSV file with synthetic data.
+    """)
+    return
+
+
+@app.cell
+def _(mf, mo):
+    from metasyn.file import CsvFileInterface
+    from io import BytesIO
+
+    def _gen_csv(mf):
+        handler = BytesIO()
+        mf.write_synthetic(handler, file_format=CsvFileInterface.default_interface("synthetic.csv").to_dict(), progress_bar=False)
+        return handler
+
+    mo.download(
+        data=_gen_csv(mf),
+        filename="synthetic.csv",
+        mimetype="text/csv",
+        label="Generate CSV",
+    )
     return
 
 
